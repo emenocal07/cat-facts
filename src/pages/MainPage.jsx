5b7e569dcaca258206871ad65b6b04988996bfb3
Ernesto Espinoza M <emenocal07@gmail.com>
2022-04-07 12:27:03 +0200

    add useEffect

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Title from '../components/Title/Title';
 import axios from 'axios';
 import { Button, Col, Row, Spin } from 'antd';
@@ -9,32 +9,36 @@ const MainPage = () => {
     const [fact, setFact] = useState('');
     const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        getFact()
+    }, [])
+
+
     const getFact = () => {
         setLoading(true);
         axios.get('https://cat-fact.herokuapp.com/facts/random')
             .then(response => {
                 setFact(response.data.text);
                 setLoading(false);
-                console.log(response);
             })
             .catch(error => console.log(error))
     }
 
     return (
         <>
-            <Title/>
+            <Title />
             <Button className='button' onClick={() => getFact()} type='primary' ghost>Click me!</Button>
             {loading
                 ?
-                <p className='spinner'><Spin size="large" /> </p>
+                <div className='spinner'><Spin size="large" /> </div>
                 :
-                
+
                 <Row >
                     <Col className='fact' span={12} offset={6}>
                         <p>{fact}</p>
                     </Col>
                 </Row>}
-            
+
 
         </>
     )
